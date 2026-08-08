346aa96878bcaec5300a0e6a592dab715ffa311e
jeliuleanen8-max <julielaenen8@gmail.com>
2026-08-08 12:56:42 +0200

    Refactor ticket checking logic for readability

--- check_ticket.py
+++ check_ticket.py
@@ -1,12 +1,12 @@
 import requests
 URLS = [
-"https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=a&price=all#tickets",
-"https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=b&price=all#tickets",
-"https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=n&price=all#tickets",
+    "https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=a&price=all#tickets",
+    "https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=b&price=all#tickets",
+    "https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=n&price=all#tickets",
 ]
 for url in URLS:
-html = requests.get(url).text
-if "No tickets available." in html:
-print(f"NO TICKETS: {url}")
-else:
-print(f"POSSIBLE TICKET FOUND: {url}")
+    html = requests.get(url).text
+    if "No tickets available." in html:
+        print(f"NO TICKETS: {url}")
+    else:
+        print(f"POSSIBLE TICKET FOUND: {url}")
