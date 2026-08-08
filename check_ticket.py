e5b91f145fc40a0774e29cd029e45ebeda0a8184
jeliuleanen8-max <julielaenen8@gmail.com>
2026-08-08 13:02:24 +0200

    Refactor ticket checking to use dictionary for URLs

--- check_ticket.py
+++ check_ticket.py
@@ -1,12 +1,26 @@
 import requests
-URLS = [
-    "https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=a&price=all#tickets",
-    "https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=b&price=all#tickets",
-    "https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=n&price=all#tickets",
-]
-for url in URLS:
-    html = requests.get(url).text
-    if "No tickets available." in html:
-        print(f"NO TICKETS: {url}")
-    else:
-        print(f"POSSIBLE TICKET FOUND: {url}")
+
+URLS = {
+    "Camping CHILL": "https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=a&price=all#tickets",
+    "Camping RELAX": "https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=b&price=all#tickets",
+    "WITHOUT camping": "https://tickets.pukkelpop.be/en/meetup/demand/?type=day2&camping=n&price=all#tickets"
+}
+
+for camping_type, url in URLS.items():
+    html = requests.get(
+        url,
+        headers={
+            "User-Agent": "Mozilla/5.0"
+        },        
+        timeout=15
+    ).text
+ 
+if "No tickets available." in html:
+    print(f"NO TICKETS ({camping_type})")
+else:
+    print("")
+    print("############################################")
+    print(f"TICKET FOUND ({camping_type})")
+    print(url)
+    print("############################################")
+    print("")
